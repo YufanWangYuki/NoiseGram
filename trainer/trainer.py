b04b2a701644b4de19ed7a0e1b1f623074d5da86
YufanWangYuki <yufanwangyuki@163.com>
2022-05-15 11:48:30 +0100

    add adv

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -98,6 +98,7 @@ class Trainer(object):
 			self.noise = np.ones([batch_size, seq_length, embedding_dim])
 		elif noise_type == 'Gaussian-adversarial':
 			self.noise = np.random.normal(1, weight, [batch_size, seq_length, embedding_dim])
+		pdb.set_trace()
 		self.noise = torch.tensor(self.noise).to(device=self.device)
 		self.noise.requires_grad = True
 		self.weight = weight
