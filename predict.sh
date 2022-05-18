9be907f40cd46c3009c7f7dbbc83765c076131a3
YufanWangYuki <yufanwangyuki@163.com>
2022-05-18 15:52:33 +0100

    debug

--- predict.sh
+++ predict.sh
@@ -27,7 +27,7 @@ input=/home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.inc
 outdir=prediction_files
 seed=1
 
-for checkpoint in 2022_05_17_01_52_19 2022_05_17_02_32_19 2022_05_17_03_12_32 2022_05_17_04_33_38
+for checkpoint in 2022_05_17_01_52_19
 do 
     output=$outdir/${exp}_${checkpoint}_seed_${seed}
     $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/predict.py \
@@ -37,3 +37,14 @@ do
         --seed $seed \
         --use_attack 0
 done
+
+# for checkpoint in 2022_05_17_01_52_19 2022_05_17_02_32_19 2022_05_17_03_12_32 2022_05_17_04_33_38
+# do 
+#     output=$outdir/${exp}_${checkpoint}_seed_${seed}
+#     $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/predict.py \
+#         --IN $input \
+#         --MODEL $model/$checkpoint/model.pt \
+#         --OUT_BASE $output \
+#         --seed $seed \
+#         --use_attack 0
+# done
