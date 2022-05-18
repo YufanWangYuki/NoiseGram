a09e26cb588880420baba019fda20287aadfe64c
YufanWangYuki <yufanwangyuki@163.com>
2022-05-18 23:57:19 +0100

    eval

--- eval_score.sh
+++ eval_score.sh
@@ -20,6 +20,7 @@ export PYTHONPATH="${PYTHONPATH}:/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/
 corr=/home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.corr
 input=/home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.inc 
 preddir=prediction_files/for_errant
+outdir=prediction_files/m2
 seed=1
 
 exp=Adversarial_mul_1.0_0.1_2_001
@@ -27,8 +28,8 @@ for checkpoint in 2022_05_17_01_52_19
 do 
     # Adversarial_mul_1.0_0.1_2_001_2022_05_17_01_52_19_seed_1.pred
     pred=$preddir/${exp}_${checkpoint}_seed_${seed}.pred
-    errant_parallel -orig $input -cor $pred -out ${exp}_${checkpoint}_edits-pred.m2
-    errant_parallel -orig $input -cor $corr -out ${exp}_${checkpoint}_edits-corr.m2
+    errant_parallel -orig $input -cor $pred -out $outdir/${exp}_${checkpoint}_edits-pred.m2
+    errant_parallel -orig $input -cor $corr -out $outdir/${exp}_${checkpoint}_edits-corr.m2
 done
 
 
