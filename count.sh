24cce37e1fe209a100dbfe2a35684c4431ce306a
YufanWangYuki <yufanwangyuki@163.com>
2022-05-20 12:18:03 +0100

    count

--- count.sh
+++ count.sh
@@ -15,7 +15,7 @@ source activate /home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37
 export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/python3
 
 # base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v2/2_Gaussian_mul_1.0_0.0/translate.txt
-base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written/lib/gec-train-bpe-written/prep/
+base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written/lib/gec-train-bpe-written/prep/test.src
 output_file=count_orig.txt
 
 # for word_keep in $(seq 0.1 0.1 1)
