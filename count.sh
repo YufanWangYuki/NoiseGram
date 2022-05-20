f09eea370441ea9f67cb2ae56d9829d98e5cf231
YufanWangYuki <yufanwangyuki@163.com>
2022-05-20 12:17:03 +0100

    count

--- count.sh
+++ count.sh
@@ -14,15 +14,16 @@ echo $CUDA_VISIBLE_DEVICES
 source activate /home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37
 export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/python3
 
-base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v2/2_Gaussian_mul_1.0_0.0/translate.txt
-output_file=count_ber.txt
+# base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v2/2_Gaussian_mul_1.0_0.0/translate.txt
+base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written/lib/gec-train-bpe-written/prep/
+output_file=count_orig.txt
 
-for word_keep in $(seq 0.1 0.1 1)
-do
-input_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v2/2_Bernoulli_mul_${word_keep}/translate.txt
+# for word_keep in $(seq 0.1 0.1 1)
+# do
+input_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v3/orig/translate.txt
 echo $input_path
 $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/count.py \
     --base_path $base_path \
     --input_path $input_path \
     --output_file $output_file
-done
+# done
