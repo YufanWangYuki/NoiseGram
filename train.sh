559d25b9992a99fbeec37c15e12bb972972652a5
YufanWangYuki <yufanwangyuki@163.com>
2022-05-24 22:45:48 +0100

    adv

--- train.sh
+++ train.sh
@@ -51,8 +51,8 @@ keep_num=5
 # --------------
 batch_size=256
 # minibatch_split=2 #8 for million
-# minibatch_split=8 #8 for million
-minibatch_split=16 #8 for million
+minibatch_split=8 #8 for million
+# minibatch_split=16 #8 for million
 num_epochs=100
 
 checkpoint_every=5000 # ~10k if 2M, batch - 256
@@ -75,10 +75,10 @@ load_mode='null' # 'resume' | 'restart' | 'null'
 # print_every=2
 
 # ----------------------- [noise] ---------------------------
-ntype=Gaussian #Gaussian, Bernoulli, Gaussian-adversarial
+ntype=Gaussian-adversarial #Gaussian, Bernoulli, Gaussian-adversarial
 nway=mul
 mean=1.0
-weight=1.5
+weight=0.1
 savedir=models/${ntype}_${nway}_${mean}_${weight}_${batch_size}_${minibatch_split}_002/
 
 # ===================================================================================
@@ -128,3 +128,7 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/train.py \
 # qsub -cwd -j yes -o 'LOGs/train_orig.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train.sh 1 1
 
 # qsub -cwd -j yes -o 'LOGs/train_gau_v1.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train.sh 1 1
+
+
+# Adv mul 0.1
+# qsub -cwd -j yes -o 'LOGs/train_adv_mul_0.1.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train.sh 1 1
