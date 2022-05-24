c218177728209260ddcac0847d2f1fef32131e7b
YufanWangYuki <yufanwangyuki@163.com>
2022-05-24 18:13:40 +0100

    split 2

--- count.sh
+++ count.sh
@@ -18,7 +18,7 @@ export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/p
 # base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written/lib/gec-train-bpe-written/prep/test.src
 # base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v3/orig/translate.txt
 base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-pretrained/models/gramformer/eval_clc-orig/beam1
-output_file=results/count_gram_edie.txt
+output_file=results/count_gram_edie_2.txt
 
 for weight in $(seq 1.1 0.1 2.5)
 do
