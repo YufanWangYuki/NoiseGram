7920ac2db27978d75c0dc8e28323d1fef0053059
YufanWangYuki <yufanwangyuki@163.com>
2022-05-25 21:43:39 +0100

    3

--- count.sh
+++ count.sh
@@ -21,7 +21,7 @@ base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-writte
 # base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v3_gramformer/orig/translate.txt
 output_file=results/count_gram_mul_trans_p3.txt
 
-for weight in 0.0
+for weight in $(seq 0.5 0.1 1)
 do
 # input_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v3/orig/translate.txt
 # input_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v3/2_Gaussian_mul_1.0_${weight}/translate.txt
