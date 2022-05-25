eb6c76ed9866955058c62139719dd38a93ebaee0
YufanWangYuki <yufanwangyuki@163.com>
2022-05-25 01:10:43 +0100

    d

--- train.sh
+++ train.sh
@@ -7,8 +7,8 @@ unset LD_PRELOAD
 echo export PATH=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/bin/:$PATH
 
 
-# export CUDA_VISIBLE_DEVICES=$X_SGE_CUDA_DEVICE
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=$X_SGE_CUDA_DEVICE
+# export CUDA_VISIBLE_DEVICES=0
 echo $CUDA_VISIBLE_DEVICES
 
 # python 3.7
@@ -51,8 +51,8 @@ keep_num=5
 # --------------
 batch_size=256
 # minibatch_split=2 #8 for million
-# minibatch_split=8 #8 for million
-minibatch_split=16 #8 for million
+minibatch_split=8 #8 for million
+# minibatch_split=16 #8 for million
 num_epochs=100
 
 checkpoint_every=5000 # ~10k if 2M, batch - 256
