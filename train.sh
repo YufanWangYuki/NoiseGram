cdadc03ee8f11911519c13d625dc7eedb13d3f47
YufanWangYuki <yufanwangyuki@163.com>
2022-05-30 20:39:00 +0100

    ad

--- train.sh
+++ train.sh
@@ -140,5 +140,5 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/train.py \
 # Gau mul 1.5
 # qsub -cwd -j yes -o 'LOGs/train_gau_mul_1.5.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train.sh 1 1
 
-# Adv
+# Pure Adv
 # qsub -cwd -j yes -o 'LOGs/train_pure_adv_mul_0.1.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train.sh 1 1
