7c7e689850c2dcfdbbbe6c4c435f284fe6a48a36
YufanWangYuki <yufanwangyuki@163.com>
2022-06-01 10:08:36 +0100

    eval error

--- eval_error.sh
+++ eval_error.sh
@@ -18,18 +18,18 @@ export PYTHONPATH="${PYTHONPATH}:/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/
 
 # for name in comma_N1 comma_N2 comma_N3 comma_N4 comma_N5 comma_N6 comma_N7 comma_N8 comma_N9
 # do
-$PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/eval_error.py \
-    --SOURCE /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.inc \
-    --REF /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.corr \
-    --PRED prediction_files/Gaussian_mul_1.0_1.5_256_2_002/orig/Gaussian_mul_1.0_1.5_256_2_002_combine_seed_1.pred \
-    --OUT edit_dist_files/Gaussian_mul_1.0_1.5_256_2_002/orig.txt
-
-
 # $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/eval_error.py \
 #     --SOURCE /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.inc \
 #     --REF /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.corr \
-#     --PRED prediction_files/Gaussian_mul_1.0_1.5_256_2_002/attacks/comma_N1_with_adv_not_removed.pred \
-#     --OUT edit_dist_files/Gaussian_mul_1.0_1.5_256_2_002/attacks/comma_N1_with_adv_not_removed.txt \
-#     --phrase 'xl' \
-#     --delim ','
+#     --PRED prediction_files/Gaussian_mul_1.0_1.5_256_2_002/orig/Gaussian_mul_1.0_1.5_256_2_002_combine_seed_1.pred \
+#     --OUT edit_dist_files/Gaussian_mul_1.0_1.5_256_2_002/orig.txt
+
+
+$PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/eval_error.py \
+    --SOURCE /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.inc \
+    --REF /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.corr \
+    --PRED prediction_files/Gaussian_mul_1.0_1.5_256_2_002/attacks/comma_N1_with_adv_not_removed.pred \
+    --OUT edit_dist_files/Gaussian_mul_1.0_1.5_256_2_002/attacks/comma_N1_with_adv_not_removed.txt \
+    --phrase 'xl' \
+    --delim ','
 # done
