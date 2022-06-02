0247feeed63babfcded45cfa62a1e093343bec68
YufanWangYuki <yufanwangyuki@163.com>
2022-06-02 02:23:26 +0100

    eval adv train

--- trainer/trainer3.py
+++ trainer/trainer3.py
@@ -249,7 +249,7 @@ class Trainer(object):
 
 				with torch.no_grad():
 					incre_noise = self.weight * norm_grad * torch.full([self.minibatch_size, self.seq_length, self.embedding_dim],1).to(device=self.device)
-					c += incre_noise
+					self.noise += incre_noise
 				pdb.set_trace()
 				
 				outputs = model.forward_train(src_ids, src_att_mask, tgt_ids, noise_configs, self.noise)
