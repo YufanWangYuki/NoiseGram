d3abab5dace9b921f0b9831b6291dcee9acde54a
YufanWangYuki <yufanwangyuki@163.com>
2022-06-04 09:07:55 +0100

    adv

--- count.sh
+++ count.sh
@@ -21,11 +21,11 @@ base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-te
 # base_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written/lib/gec-train-bpe-written/prep/test.src
 # base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v001/eval-clc-test-beam-1/combine_v3_gramformer/orig/translate.txt
 # base_path=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v002/Gaussian_mul_1.0_1.5_256_2_002/eval-clc-test-beam-1/combine_Gau_1.5/orig/translate.txt
-output_file=results/adv/count_trans_single.txt
+output_file=results/adv/count_adv_trans.txt
 
 # for ntype in Gaussian-adversarial Adversarial
 # do
-for ntype in Gaussian-adversarial-single Adversarial-single
+for ntype in Gaussian-adversarial Adversarial
 do
 for weight in 0.0 0.001 0.005 0.01 0.05 0.1 0.5 1 1.5
 do
@@ -52,3 +52,5 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/count.py \
     --output_file $output_file
 done
 done
+
+# qsub -cwd -j yes -o 'LOGs/count/count_single.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' count.sh 1 1
