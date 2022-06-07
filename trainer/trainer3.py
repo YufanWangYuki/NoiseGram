958e0b8b85138d6a7b1264ad3c17b520f2942d02
YufanWangYuki <yufanwangyuki@163.com>
2022-06-07 13:24:14 +0100

    debug

--- trainer/trainer3.py
+++ trainer/trainer3.py
@@ -239,9 +239,8 @@ class Trainer(object):
 
 			# Forward propagation
 			if "dversarial" in noise_configs['noise_type']:
-				# model.eval()
-				with torch.no_grad():
-					outputs = model.forward_train(src_ids, src_att_mask, tgt_ids, noise_configs, self.noise)
+				model.eval()
+				outputs = model.forward_train(src_ids, src_att_mask, tgt_ids, noise_configs, self.noise)
 				loss = outputs.loss
 				loss /= n_minibatch
 				
