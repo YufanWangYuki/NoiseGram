2ac173e73cc17850310b720196039de108aaa353
YufanWangYuki <yufanwangyuki@163.com>
2022-06-08 13:18:31 +0100

    debug

--- train.sh
+++ train.sh
@@ -69,8 +69,8 @@ train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
 dev_path_src=$orig_path/lib/gec-train-bpe-written/prep/toy.src
 dev_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/toy.tgt
 # # num_epochs=2
-minibatch_split=4
-batch_size=8
+minibatch_split=2
+batch_size=4
 checkpoint_every=100
 print_every=2
 
@@ -161,4 +161,4 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/train.py \
 # qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.01_split16.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train0.01.sh 1 1
 # qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.01_split8_v.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train.sh 1 1
 
-qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.0_v.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train.sh 1 1
+# qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.0_v.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train.sh 1 1
