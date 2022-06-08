2b97f92458f055bad842e8ba9239d15b705b6d97
YufanWangYuki <yufanwangyuki@163.com>
2022-06-08 01:18:51 +0100

    0.01

--- train.sh
+++ train.sh
@@ -54,7 +54,7 @@ batch_size=256
 # minibatch_split=2 #8 for million
 # minibatch_split=8 #8 for million
 # minibatch_split=8 #8 for million
-minibatch_split=16 #8 for million
+minibatch_split=32 #8 for million
 num_epochs=100
 
 checkpoint_every=5000 # ~10k if 2M, batch - 256
@@ -160,4 +160,4 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/train.py \
 # qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.01.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train0.01.sh 1 1
 # qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.1_split8.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train.sh 1 1
 # qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.01_split16.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' train0.01.sh 1 1
-# qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.01_split16_v.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train.sh 1 1
+# qsub -cwd -j yes -o 'LOGs/adv_updated/train_adv_mul_0.01_split32_v.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='volta' -l osrel='*' train.sh 1 1
