c2597e23617c13a46beebf6a22b0ea09992a41cd
YufanWangYuki <yufanwangyuki@163.com>
2022-06-10 00:28:20 +0100

    init

--- trainer/trainer6.py
+++ trainer/trainer6.py
@@ -295,6 +295,7 @@ class Trainer(object):
 		print(torch.mean(self.noise))
 		print(torch.var(self.noise))
 		self.noise = self.noise.expand([self.minibatch_size,self.seq_length,self.embedding_dim])
+		self.noise.requires_grad = True
 		pdb.set_trace()
 		# time.sleep(1)
 		return resloss
