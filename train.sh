83a7359964c0928ebc4591843769d370326c09dc
YufanWangYuki <yufanwangyuki@163.com>
2022-06-10 15:19:21 +0100

    mul

--- train.sh
+++ train.sh
@@ -78,7 +78,7 @@ load_mode='null' # 'resume' | 'restart' | 'null'
 ntype=Gaussian-adversarial #Gaussian, Bernoulli, Gaussian-adversarial, Adversarial
 nway=add
 mean=0.0
-weight=0.1
+weight=0.01
 savedir=models/v004/volta_${ntype}_${nway}_${mean}_${weight}_${batch_size}_${minibatch_split}/
 
 # ===================================================================================
