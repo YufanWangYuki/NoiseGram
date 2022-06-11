06d672c8703c8b95b14bc75d7c70d63986bc0aaa
YufanWangYuki <yufanwangyuki@163.com>
2022-06-11 23:44:21 +0100

    adv

--- trainer/trainer7.py
+++ trainer/trainer7.py
@@ -256,11 +256,11 @@ class Trainer(object):
 				new_noise = self.noise + self.alpha * grad
 				# pdb.set_trace()
 
-				# with torch.no_grad():
-				# 	for i in range(len(src_ids)):
-				# 		new_noise[i] /= (torch.norm(new_noise[i]) + 1e-10)
-				# 		new_noise[i] *= self.weight
-				# pdb.set_trace()
+				with torch.no_grad():
+					for i in range(len(src_ids)):
+						new_noise[i] /= (torch.norm(new_noise[i]) + 1e-10)
+						new_noise[i] *= self.weight
+				pdb.set_trace()
 
 				# Second forward propagation-get loss
 				model.train()
@@ -296,6 +296,7 @@ class Trainer(object):
 		self.optimizer.step()
 		model.zero_grad()
 		noise_bar /= batch_size
+		pdb.set_trace()
 		with torch.no_grad():
 			self.noise = self.gamma * self.noise + (1 - self.gamma) * noise_bar.expand([self.minibatch_size,self.seq_length,self.embedding_dim])
 		# pdb.set_trace()
