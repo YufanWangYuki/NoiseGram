53d5eac721c8e4bc20231b592b1c3afbea7ba614
YufanWangYuki <yufanwangyuki@163.com>
2022-06-12 18:50:57 +0100

    adv

--- trainer/trasner_adv7.py
+++ trainer/trasner_adv7.py
@@ -274,9 +274,10 @@ class Trainer(object):
 				outputs = model.forward_train(src_ids, src_att_mask, tgt_ids, noise_configs, self.noise)
 				loss = outputs.loss
 				loss /= n_minibatch
+				pdb.set_trace()
 
 				grad = torch.autograd.grad(loss, self.noise, retain_graph=True, create_graph=False)[0]
-				pdb.set_trace()
+				
 				if "norm" in noise_configs['noise_type']:
 					print("norm")
 					with torch.no_grad():
