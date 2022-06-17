38042405e1218801dc13cc134cc8835695833a8e
YufanWangYuki <yufanwangyuki@163.com>
2022-06-17 17:22:02 +0100

    eval

--- eval_score.sh
+++ eval_score.sh
@@ -145,19 +145,19 @@ seed=1
 # done
 # done
 
-outdir=prediction_files/m2/v003_volta_Gaussian-adversarial_mul_1.0_0.1_256_8
-mkdir prediction_files/m2/v003_volta_Gaussian-adversarial_mul_1.0_0.1_256_8
-for exp in v003_volta_Gaussian-adversarial_mul_1.0_0.1_256_8
+outdir=prediction_files/m2/v005
+mkdir prediction_files/m2/v005
+for exp in volta_Gaussian-adversarial_add_0.0_1000_256_8 volta_Gaussian-adversarial_add_0.0_100_256_8 volta_Gaussian-adversarial-norm_add_0.0_100_256_8 volta_Gaussian-adversarial-norm_add_0.0_10_256_8 
 do
 checkpoint=combine
-dir=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/for_errant/$exp
+dir=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/for_errant/v005/$exp
 input=$dir/combine_seed_1.inc
 pred=$dir/combine_seed_1.pred
 corr=$dir/combine_seed_1.corr
 errant_parallel -orig $input -cor $pred -out $outdir/${exp}_combine_edits-pred.m2
 errant_parallel -orig $input -cor $corr -out $outdir/${exp}_combine_edits-corr.m2
-    echo ${exp}_${checkpoint} >> results/Fscore/${exp}_Fscore.txt
-    errant_compare -hyp $outdir/${exp}_${checkpoint}_edits-pred.m2 -ref $outdir/${exp}_${checkpoint}_edits-corr.m2 >> results/Fscore/${exp}_Fscore.txt
+    echo ${exp}_${checkpoint} >> results/Fscore/v005_Fscore.txt
+    errant_compare -hyp $outdir/${exp}_${checkpoint}_edits-pred.m2 -ref $outdir/${exp}_${checkpoint}_edits-corr.m2 >> results/Fscore/v005_Fscore.txt
     echo ${exp}_${checkpoint}
 done
 
