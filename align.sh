d0eb7f11300c0fb4e9ac0d46d6d18e0e248805d7
YufanWangYuki <yufanwangyuki@163.com>
2022-07-05 00:27:57 +0100

    mul

--- align.sh
+++ align.sh
@@ -150,7 +150,7 @@ seed=1
 
 preddir=prediction_files/v005
 # mkdir $outdir/orig
-for exp in volta_Gaussian-adversarial_add_0.0_1000_100000000_0.1_256_8 volta_Gaussian-adversarial_add_0.0_1000_100000000_0.1_4_2
+for exp in volta_Gaussian-adversarial_mul_1.0_0.1_10000000_1_256_8
 do
 mkdir $outdir/v005/${exp}
     checkpoint=combine
