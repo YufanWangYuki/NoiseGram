411f378e1a438cf8921a7ee0285599d24703f1c5
YufanWangYuki <yufanwangyuki@163.com>
2022-07-07 22:48:13 +0100

    test

--- train.sh
+++ train.sh
@@ -7,8 +7,8 @@ unset LD_PRELOAD
 echo export PATH=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/bin/:$PATH
 
 
-export CUDA_VISIBLE_DEVICES=$X_SGE_CUDA_DEVICE
-# export CUDA_VISIBLE_DEVICES=0
+# export CUDA_VISIBLE_DEVICES=$X_SGE_CUDA_DEVICE
+export CUDA_VISIBLE_DEVICES=0
 echo $CUDA_VISIBLE_DEVICES
 
 # python 3.7
@@ -66,24 +66,24 @@ loaddir='None'
 load_mode='null' # 'resume' | 'restart' | 'null'
 
 # ----------------------- [debug] ---------------------------
-# train_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src
-# train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
-# dev_path_src=$orig_path/lib/gec-train-bpe-written/prep/toy.src
-# dev_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/toy.tgt
-# # # num_epochs=2
-# minibatch_split=2
-# batch_size=4
-# checkpoint_every=100
-# print_every=2
+train_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src
+train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
+dev_path_src=$orig_path/lib/gec-train-bpe-written/prep/toy.src
+dev_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/toy.tgt
+# # num_epochs=2
+minibatch_split=2
+batch_size=4
+checkpoint_every=100
+print_every=2
 
 # ----------------------- [noise] ---------------------------
-ntype=Gaussian #Gaussian, Bernoulli, Gaussian-adversarial, Adversarial
+ntype=Gaussian-adversarial #Gaussian, Bernoulli, Gaussian-adversarial, Adversarial
 nway=mul
 mean=1.0
-weight=0.0
+weight=0.1
 # alpha=1
 # decay=1
-savedir=models/orig/volta_${ntype}_${nway}_${mean}_${weight}_${decay}_${batch_size}_${minibatch_split}/
+savedir=models/temp/volta_${ntype}_${nway}_${mean}_${weight}_${decay}_${batch_size}_${minibatch_split}/
 
 # ===================================================================================
 $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/train_orig.py \
@@ -120,7 +120,7 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/train_orig.py \
 	--nway $nway \
 	--mean $mean \
 	--weight $weight \
-	# --alpha $alpha
+	--alpha $alpha
 
 # Run below command to submit this script as an array job
 # qsub -cwd -j yes -P esol -l qp=low -o LOGs/train.txt -t 1-5 -l not_host="air113|air116" train.sh 1
