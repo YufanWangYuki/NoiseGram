4c67d9163dd9bf39086f0119e186d777df263136
YufanWangYuki <yufanwangyuki@163.com>
2022-07-08 23:52:52 +0100

    prep

--- run_search_evade_perp.sh
+++ run_search_evade_perp.sh
@@ -11,8 +11,9 @@ export MKL_NUM_THREADS=1 # export MKL_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1 # export VECLIB_MAXIMUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1 # export NUMEXPR_NUM_THREADS=1
 
+SGE_TASK_ID=0
 
-python /home/alta/BLTSpeaking/exp-vr313/GEC/TunedGramformerAttack/uni_attack_evade_perplexity.py /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.train16.inc /home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written/models/v001/checkpoints-combine/combine/model-dict.pt /home/alta/BLTSpeaking/grd-graphemic-vr313/speech_processing/adversarial_attack/word2vec/test_words.txt /home/alta/BLTSpeaking/exp-vr313/GEC/TunedGramformerAttack/universal_attack_logs/evade_perp_beam1/k9/words$SGE_TASK_ID.txt --prev_attack='trifecta haiku utah intransigent penicillin baseline exploratory bioengineering' --search_size=200 --start=$SGE_TASK_ID --num_points=500 --perp_thresh=243
+python /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/uni_attack_evade_perplexity.py /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.train16.inc /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v003/volta_Gaussian-adversarial_mul_1.0_0.1_256_8/checkpoints-combine/combine/model.pt /home/alta/BLTSpeaking/grd-graphemic-vr313/speech_processing/adversarial_attack/word2vec/test_words.txt /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/universal_attack_logs/evade_perp_beam1/k1/words$SGE_TASK_ID.txt --prev_attack= --search_size=200 --start=$SGE_TASK_ID --num_points=500 --perp_thresh=243
 
 # Run below command to submit this script as an array job
 # qsub -cwd -j yes -P esol -l qp=low -o LOGs/run-array-evade_perp.txt -t 1-224 -l not_host="air113|air112" run_search_evade_perp.sh
