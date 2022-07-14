a2048cf47f1fe4557049f7edebe45c7b3d8d6166
YufanWangYuki <yufanwangyuki@163.com>
2022-07-14 09:57:26 +0100

    0

--- train.py
+++ train.py
@@ -17,7 +17,7 @@ from models.Seq2seq import Seq2seq
 # from trainer.trainer5 import Trainer
 # from trainer.trainer6 import Trainer
 # from trainer.trainer7 import Trainer
-from trainer.trainer8 import Trainer
+from trainer.trainer9 import Trainer
 
 def load_arguments(parser):
 
