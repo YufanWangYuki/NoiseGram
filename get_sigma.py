67bbfe4e5df273934041fd75701c2f023251cc8c
YufanWangYuki <yufanwangyuki@163.com>
2022-07-20 14:54:55 +0100

    get sigma

--- get_sigma.py
+++ get_sigma.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 
-file_dir_v1 = '/Users/yufanwang/Desktop/Study/Project/Data/backup/save_tensor/'
+file_dir_v1 = '/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v008/save_tensor/'
 file_list = sorted(os.listdir(file_dir_v1))
 idx_list_v1 = []
 for file in file_list:
@@ -14,7 +14,7 @@ for file in file_list:
     idx_list_v1.append(int(file[:-3]))
 # print(sorted(idx_list_v1))
 
-file_dir_v2 = '/Users/yufanwang/Desktop/Study/Project/Data/backup/save_tensor_v2/'
+file_dir_v2 = '/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/models/v008/save_tensor_v2/'
 file_list = sorted(os.listdir(file_dir_v2))
 idx_list_v2 = []
 for file in file_list:
@@ -30,8 +30,10 @@ for i in idx_list_v2:
 
 mean_sigma = []
 for id in tqdm(final_idx):
-    data_v1 = torch.load(file_dir_v1+str(id)+".pt",map_location=torch.device('cpu'))-1
-    data_v2 = torch.load(file_dir_v2+str(id)+".pt",map_location=torch.device('cpu'))-1
+    # data_v1 = torch.load(file_dir_v1+str(id)+".pt",map_location=torch.device('cpu'))-1
+    # data_v2 = torch.load(file_dir_v2+str(id)+".pt",map_location=torch.device('cpu'))-1
+    data_v1 = torch.load(file_dir_v1+str(id)+".pt")-1
+    data_v2 = torch.load(file_dir_v2+str(id)+".pt")-1
     matrix = data_v1 / data_v2*0.1
     mean_sigma.append(float(torch.mean(matrix).float()))
 
@@ -56,7 +58,9 @@ for i in range(len(hist[0])):
         if hist[0][i] == max(hist[0]):
             plt.annotate(text=hist[1][i], xy=(hist[1][i] + 0.1 / 3, hist[0][i]))
 plt.xlim([-10,20])
+plt.savefig("sigma.png")
 plt.show()
+
 
 
 
