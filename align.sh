600883a59ef5dd4f507c5c42f4a0b02727c249d2
YufanWangYuki <yufanwangyuki@163.com>
2022-07-20 23:36:47 +0100

    conll

--- align.sh
+++ align.sh
@@ -193,9 +193,10 @@ do
     checkpoint=combine
     pred=$preddir/${exp}_combine_seed_1.pred
     output=$outdir/$exp/${checkpoint}_seed_${seed}
-    corr=/home/alta/BLTSpeaking/exp-vr313/GEC/data/CoNLL-14/conll14st-test-data/noalt/official-2014.combined.m2
+    
     echo $pred
     input=/home/alta/BLTSpeaking/exp-vr313/GEC/data/CoNLL-14/conll14st-test-data/noalt/input_sentences.txt
+    corr=input
     $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/utils/align_preds.py \
         --INC $input \
         --PRED $pred \
