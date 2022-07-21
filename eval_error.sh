9bf5c721030fbb770d46fb6aa26267acc45113a5
YufanWangYuki <yufanwangyuki@163.com>
2022-07-21 01:39:28 +0100

    conll

--- eval_error.sh
+++ eval_error.sh
@@ -423,7 +423,7 @@ mkdir edit_dist_files/orig
 for exp in CoNLL_volta_Gaussian_mul_1.0_0.0__256_8_combine_seed_1
 do
     dir=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/orig/attacks
-    input=/home/alta/BLTSpeaking/exp-vr313/GEC/data/CoNLL-14/conll14st-test-data/noalt/input_sentences.txt
+    input=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/orig/clean_input_sentences.txt
     
     mkdir edit_dist_files/orig/$exp
     outdir=edit_dist_files/orig/$exp
@@ -432,7 +432,7 @@ do
     name=full_N5
     ref=$dir/volta_Gaussian_mul_1.0_0.0__256_8_full_N5_CoNLL_with_adv_not_removed.pred
 $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/eval_error.py \
-    --SOURCE  $input\
+    --SOURCE $input\
     --REF $ref\
     --PRED $ref \
     --OUT ${outdir}/${name}_with_adv_not_removed.txt \
