5c9fac3f9a50f22937b6c0ef1676b2dd2593c9f2
YufanWangYuki <yufanwangyuki@163.com>
2022-07-21 23:58:43 +0100

    vyas perp

--- get_best.sh
+++ get_best.sh
@@ -13,11 +13,10 @@ source ~/.bashrc
 source activate /home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37
 export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/python3
 export PYTHONPATH="${PYTHONPATH}:/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/"
-
+# /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/universal_attack_logs/v003best/evade_perp_beam1/k${num}/ \
 for num in 1 2 3 4 5 6 7 8 9
 do
 $PYTHONBIN get_best.py \
-    # /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/universal_attack_logs/v003best/evade_perp_beam1/k${num}/ \
     /home/alta/BLTSpeaking/exp-vr313/GEC/TunedGramformerAttack/universal_attack_logs/evade_perp_beam1/k${num}/ \
     /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/LOGs/best_words/vyas_best_words_${num}.txt
 done
