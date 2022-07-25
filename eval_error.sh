a29834ba71bef2a1d878947ae597acf14677dd98
YufanWangYuki <yufanwangyuki@163.com>
2022-07-25 23:36:54 +0100

    k6

--- eval_error.sh
+++ eval_error.sh
@@ -273,7 +273,7 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/eval_error.py \
     --REF /home/alta/CLC/LNRC/exams/FCEsplit-public/v3/fce-public.test.corr \
     --PRED ${pred_dir}/${name}_with_adv_not_removed.pred \
     --OUT ${out_dir}/${name}_with_adv_not_removed.txt \
-    --phrase 'chutzpah vb clap shu wring sg' \
+    --phrase 'chutzpah vb clap shu wring tangy' \
     --delim '.'
 
 # name=full_N7
