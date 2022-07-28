aef28e9af92e50b9077dd67dab11c226df006985
YufanWangYuki <yufanwangyuki@163.com>
2022-07-28 19:29:49 +0100

    100

--- eval_conll_bea.sh
+++ eval_conll_bea.sh
@@ -17,13 +17,13 @@ export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/p
 export PYTHONPATH="${PYTHONPATH}:/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/"
 
 
-for exp in v002
+for exp in generate
 do
     dir=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/orig
     pred=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/for_errant/conll/$exp
-    errant_parallel -orig ${dir}/conlltest.inc -cor ${pred}/clean_CoNLL_Gaussian_1.5.pred -out ${pred}/clean_${exp}_1.5.m2
-    echo clean_${exp}_1.5 >> results/Fscore/v002_conll.txt
-    errant_compare -hyp ${pred}/clean_${exp}_1.5.m2 -ref ${dir}/official-2014.combined.m2 >> results/Fscore/v002_conll.txt
+    errant_parallel -orig ${dir}/conlltest.inc -cor ${pred}/clean_CoNLL_generate.pred -out ${pred}/clean_${exp}.m2
+    echo clean_${exp} >> results/Fscore/v002_conll.txt
+    errant_compare -hyp ${pred}/clean_${exp}.m2 -ref ${dir}/official-2014.combined.m2 >> results/Fscore/v002_conll.txt
 done
 
 # errant_compare -hyp edits-pred.m2 -ref /home/alta/BLTSpeaking/exp-vr313/GEC/data/CoNLL-14/conll14st-test-data/noalt/official-2014.combined.m2
