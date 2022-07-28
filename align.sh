16214b43878547849331e8b9e35196289f70a5e6
YufanWangYuki <yufanwangyuki@163.com>
2022-07-28 12:17:14 +0100

    v003

--- align.sh
+++ align.sh
@@ -191,8 +191,8 @@ do
     preddir=/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/prediction_files/conll
     mkdir $outdir/$exp
     checkpoint=combine
-    pred=$preddir/CoNLL_v003_combine_seed_1.pred
-    output=$outdir/$exp/${checkpoint}_seed_${seed}
+    pred=$preddir/clean_CoNLL_v003_combine_seed_1.pred
+    output=$outdir/$exp/clean_${checkpoint}_seed_${seed}
     
     echo $pred
     input=/home/alta/BLTSpeaking/exp-vr313/GEC/data/CoNLL-14/conll14st-test-data/noalt/input_sentences.txt
