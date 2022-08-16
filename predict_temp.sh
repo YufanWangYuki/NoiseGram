faa8db1ab1ed2fbf4dc17578902e70c56c9caf54
YufanWangYuki <yufanwangyuki@163.com>
2022-08-16 17:59:20 +0100

    pred temp

--- predict_temp.sh
+++ predict_temp.sh
@@ -3,7 +3,7 @@
 
 echo $HOSTNAME
 unset LD_PRELOAD
-echo export PATH=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/bin/:$PATH
+echo export PATH=/home/mifs/yw575/env/anaconda3/envs/gec37/bin:$PATH
 
 # export CUDA_VISIBLE_DEVICES=$X_SGE_CUDA_DEVICE
 export CUDA_VISIBLE_DEVICES=0
@@ -12,8 +12,8 @@ echo $CUDA_VISIBLE_DEVICES
 # python 3.7
 # pytorch 1.5
 source activate /home/mifs/yw575/env/anaconda3/envs/gec37
-# export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/python3
-# export PYTHONPATH="${PYTHONPATH}:/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/"
+export PYTHONBIN=/home/mifs/yw575/env/anaconda3/envs/gec37/bin/python3
+export PYTHONPATH="${PYTHONPATH}:/home/mifs/yw575/NoiseGram/"
 
 # ===================================================================================
 input=/home/mifs/yw575/dataset/test.src 
